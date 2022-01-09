d57b6df5edea7d58de83a772c938378a8949d10e
RichardKim <hiddeneventor@gmail.com>
2022-01-09 14:33:02 +0900

    spring cloud 세팅 및 Netty 비동기 방식 서비스 구성 확인완료

---
+++ apigatway-service/src/main/java/com/example/apigatwayservice/ApigatwayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatwayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatwayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatwayServiceApplication.class, args);
+    }
+
+}
