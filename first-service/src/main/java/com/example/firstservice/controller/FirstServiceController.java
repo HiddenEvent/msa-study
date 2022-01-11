ce4bf6f84bf380594d16d48147154e3a09afbaf4
RichardKim <hiddeneventor@gmail.com>
2022-01-12 00:54:42 +0900

    로드 벨런싱 기능 테스트 완료 - [apiGateway] yml에 uri 설정을 lb://서비스이름 설정 - [First-service] 로드벨런싱 설정 -> yml에 랜덤포트 설정 (port:0 => instance 랜덤포트 설정)

--- first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,14 +1,22 @@
 package com.example.firstservice.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    private final Environment env;
+
     @GetMapping("/welcom")
     public String welcom() {
         return "Welcom to the First Service.";
@@ -21,8 +29,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there first";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format( "Hi, there first First Service on PORT %s", env.getProperty("local.server.port"));
     }
 
 }
