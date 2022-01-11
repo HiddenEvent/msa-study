51e0b47f42922f0c266180a47b27bb98fa13927f
RichardKim <hiddeneventor@gmail.com>
2022-01-11 23:51:46 +0900

    yml 설정을 통한 Global/Custom Filter 적용 ※ 참고 yml <=> java config 파일 둘중 java config가 먼저 적용된다

--- apigatway-service/src/main/java/com/example/apigatwayservice/config/FilterConfig.java
+++ apigatway-service/src/main/java/com/example/apigatwayservice/config/FilterConfig.java
@@ -15,37 +15,37 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Slf4j
-@Configuration
+//@Slf4j
+//@Configuration
 public class FilterConfig {
 
-
-    @Bean
-    public RouteLocator getewayRoutes(RouteLocatorBuilder builder) {
-
-        return builder.routes()
-                .route(r -> r.path("/first-service/**")
-                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
-                                .addResponseHeader("first-response", "first-response-header")
-                                .filter(this::customFilter)
-                        )
-                        .uri("http://localhost:8081"))
-                .route(r -> r.path("/second-service/**")
-                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
-                                .addResponseHeader("second-response", "second-response-header")
-                                .filter(this::customFilter)
-                        )
-                        .uri("http://localhost:8082"))
-                .build();
-    }
-
-    private Mono<Void> customFilter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        ServerHttpResponse response = exchange.getResponse();
-        log.info("커스텀 PRE filter: request id -> {}", request.getId());
-
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            log.info("커스텀 POST filter: response id -> {}", response.getStatusCode());
-        }));
-    }
+// global 필터 적용 어떻게 하는지 추후 확인
+//    @Bean
+//    public RouteLocator getewayRoutes(RouteLocatorBuilder builder) {
+//
+//        return builder.routes()
+//                .route(r -> r.path("/first-service/**")
+//                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+//                                .addResponseHeader("first-response", "first-response-header")
+//                                .filter(this::customFilter)
+//                        )
+//                        .uri("http://localhost:8081"))
+//                .route(r -> r.path("/second-service/**")
+//                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+//                                .addResponseHeader("second-response", "second-response-header")
+//                                .filter(this::customFilter)
+//                        )
+//                        .uri("http://localhost:8082"))
+//                .build();
+//    }
+//
+//    private Mono<Void> customFilter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        ServerHttpRequest request = exchange.getRequest();
+//        ServerHttpResponse response = exchange.getResponse();
+//        log.info("커스텀 PRE filter: request id -> {}", request.getId());
+//
+//        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//            log.info("커스텀 POST filter: response id -> {}", response.getStatusCode());
+//        }));
+//    }
 }
