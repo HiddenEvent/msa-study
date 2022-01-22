fa984567f275d504bbc11968eb23fddb9e46f941
RichardKim <hiddeneventor@gmail.com>
2022-01-22 16:47:01 +0900

    SF-34 RestTemplate 사용 - @LoadBalanced // 마이크로 서비스 이름을 가지고 호출하기 위한 설정 (Eureka 대시보드 이름으로 명시) - Config 프로젝트에서 url 호출 방식을 서비스 이름으로 호출할 수 있게 변경

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -29,6 +30,7 @@ public class UserServiceApplication {
         return mapper;
     }
     @Bean
+    @LoadBalanced // 마이크로 서비스 이름을 가지고 호출하기 위한 설정 (Eureka 대시보드 이름으로 명시)
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
